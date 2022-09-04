bc94c2ffed5595914287c5f220365195ad2de1a7
unknown <azkiawardani7@gmail.com>
2022-09-04 11:30:26 +0700

    update

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hii Im coming!");
+let age = 23;
+age = 23;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log("Hii Im coming!");
+
+let age : number = 23
+
+age = 23  //true
+// age ="asa" //false
